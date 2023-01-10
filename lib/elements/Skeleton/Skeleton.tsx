616e69b395b417966c65b58e2d77d0dc45473e75
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-10 19:45:31 +0200

    wtff

--- lib/elements/Skeleton/Skeleton.tsx
+++ lib/elements/Skeleton/Skeleton.tsx
@@ -21,12 +21,18 @@ import Animated, {
  */
 export const Skeleton: FC<{ children: ReactNode }> = ({ children }) => {
   const opacity = useSharedValue(0.5)
-  const fadeLoopAnim = useMemo(() => useAnimatedStyle(() => ({ opacity: opacity.value })), [])
+  // const fadeLoopAnim = useAnimatedStyle(() => ({ opacity: opacity.value }))
   // useEffect(() => {
   // opacity.value = withRepeat(withTiming(1, { duration: 1000, easing: Easing.ease }), -1, true)
   // }, [])
 
-  return <Animated.View style={fadeLoopAnim}>{children}</Animated.View>
+  return (
+    <Animated.View
+    //  style={fadeLoopAnim}
+    >
+      {children}
+    </Animated.View>
+  )
 }
 
 export const SkeletonText: FC<TextProps> = ({ children, ...rest }) => {
