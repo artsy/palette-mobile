6e14fc6848c8f8a858c9d5e4a372f4b0d5192a44
Pavlos Vinieratos <pvinis@gmail.com>
2023-02-02 18:44:13 +0000

    Update AddCircleFillIcon.tsx

--- lib/svgs/AddCircleFillIcon.tsx
+++ lib/svgs/AddCircleFillIcon.tsx
@@ -13,6 +13,3 @@ export const AddCircleFillIcon = ({ fill, ...restProps }: IconProps) => {
     </Icon>
   )
 }
-
-// TODO: remove this alias once clients have been updated
-export const PlusIcon = AddCircleFillIcon
