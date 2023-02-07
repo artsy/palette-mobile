9afd0f468d1bb34788756b31ababa211e1e53661
Pavlos Vinieratos <pvinis@gmail.com>
2023-02-07 22:28:58 +0000

    hm

--- .storybook/storybook.requires.js
+++ .storybook/storybook.requires.js
@@ -5,11 +5,20 @@ import {
   addDecorator,
   addParameters,
   addArgsEnhancer,
+  clearDecorators,
 } from "@storybook/react-native";
 
 import { decorators, parameters } from "./preview";
 
 if (decorators) {
+  if (__DEV__) {
+    // stops the warning from showing on every HMR
+    require("react-native").LogBox.ignoreLogs([
+      "`clearDecorators` is deprecated and will be removed in Storybook 7.0",
+    ]);
+  }
+  // workaround for global decorators getting infinitely applied on HMR, see https://github.com/storybookjs/react-native/issues/185
+  clearDecorators();
   decorators.forEach((decorator) => addDecorator(decorator));
 }
 
