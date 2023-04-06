74b5d64076225e5708443d72df13d83b2a3c748b
Christopher Pappas <damassi.pappas@gmail.com>
2023-04-06 16:20:03 -0700

    finish moving stuff

--- src/elements/ToolTip/ToolTip.tests.tsx
+++ src/elements/ToolTip/ToolTip.tests.tsx
@@ -1,21 +1,26 @@
 import { ToolTip } from "./ToolTip"
+import { ScreenDimensionsProvider } from "../../utils/hooks"
 import { renderWithWrappers } from "../../utils/tests/renderWithWrappers"
 import { Text } from "../Text"
 
 describe("ToolTip", () => {
   it("shows the flyout when enabled", () => {
     const { queryByTestId } = renderWithWrappers(
-      <ToolTip enabled testID="flyout" initialToolTipText="Words">
-        <Text>Text</Text>
-      </ToolTip>
+      <ScreenDimensionsProvider>
+        <ToolTip enabled testID="flyout" initialToolTipText="Words">
+          <Text>Text</Text>
+        </ToolTip>
+      </ScreenDimensionsProvider>
     )
     expect(queryByTestId("flyout")).not.toBeNull()
   })
   it("Does not show the flyout when disabled", () => {
     const { queryByTestId } = renderWithWrappers(
-      <ToolTip enabled={false} testID="flyout" initialToolTipText="Words">
-        <Text>Text</Text>
-      </ToolTip>
+      <ScreenDimensionsProvider>
+        <ToolTip enabled={false} testID="flyout" initialToolTipText="Words">
+          <Text>Text</Text>
+        </ToolTip>
+      </ScreenDimensionsProvider>
     )
     expect(queryByTestId("flyout")).toBeNull()
   })
