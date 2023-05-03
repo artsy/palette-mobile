2f91c1a46c5a4f62f8403b361c9080bfd53ee265
Christopher Pappas <damassi.pappas@gmail.com>
2023-05-03 13:29:25 -0700

    fix(import): fix super relative imports

--- src/elements/Button/FollowButton.tsx
+++ src/elements/Button/FollowButton.tsx
@@ -1,5 +1,5 @@
-import { Button, ButtonProps } from "."
 import { CheckIcon } from "../../svgs/CheckIcon"
+import { Button, ButtonProps } from "../Button"
 
 type FollowButtonProps = Omit<
   ButtonProps,
