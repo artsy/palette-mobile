07f40fa8afdc048651e5d76aa342a760749db6e2
Mounir Dhahri <soussouheros@gmail.com>
2024-01-22 14:00:29 +0100

    refactor: rename prop

--- src/elements/Screen/StickySubHeader.tsx
+++ src/elements/Screen/StickySubHeader.tsx
@@ -6,13 +6,13 @@ import { Flex } from "../Flex"
 import { Separator } from "../Separator"
 import { Text } from "../Text"
 
-export interface SubHeaderProps extends React.PropsWithChildren<{}> {
+export interface StickySubHeaderProps extends React.PropsWithChildren<{}> {
   title: string
 }
 
 const STICKY_BAR_HEIGHT = 60
 
-export const StickySubHeader: React.FC<SubHeaderProps> = ({ title, children }) => {
+export const StickySubHeader: React.FC<StickySubHeaderProps> = ({ title, children }) => {
   const { currentScrollY, scrollYOffset = 0 } = useScreenScrollContext()
 
   const visible = currentScrollY >= NAVBAR_HEIGHT + scrollYOffset ? false : true
