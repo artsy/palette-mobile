33d40c28c9386b96c5fe3feebdd2caa76cbe216a
Christopher Pappas <damassi.pappas@gmail.com>
2024-11-13 20:55:06 -0800

    feat: add expo

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    resolverMainFields: ["sbmodern", "react-native", "browser", "main"],
-  },
-}
+const { getDefaultConfig } = require("@expo/metro-config")
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+const config = getDefaultConfig(__dirname)
+
+module.exports = config
