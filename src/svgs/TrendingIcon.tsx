e903434dcb0a610518a568d115424d6c983894e4
Daria Kozlova <Nargle30@gmail.com>
2024-11-18 13:18:49 +0100

    fix(ONYX-1395): update urgency signals, follow and save icons (#282)

--- src/svgs/TrendingIcon.tsx
+++ src/svgs/TrendingIcon.tsx
@@ -5,11 +5,11 @@ export const TrendingIcon = ({ fill, ...restprops }: IconProps) => {
   const color = useColor()
 
   return (
-    <Icon {...restprops} viewBox="0 0 16 9">
+    <Icon {...restprops} viewBox="0 0 18 18">
       <Path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M13.6162 1L11.5 1V0H14.5H15.5V1V4H14.5V1.88393L8.94199 7.44194C8.69791 7.68602 8.30218 7.68602 8.05811 7.44194L5.50005 4.88388L1.44199 8.94194L0.558105 8.05806L5.05811 3.55806C5.30218 3.31398 5.69791 3.31398 5.94199 3.55806L8.50005 6.11612L13.6162 1Z"
+        d="M2.68125 13.3594L1.6875 12.375L7.07813 6.95625L10.0219 9.9L13.9313 6.04688H11.9625V4.64062H16.3125V8.99063H14.9063V7.02188L10.0313 11.9063L7.0875 8.9625L2.68125 13.3594Z"
         fill={color(fill)}
       />
     </Icon>
