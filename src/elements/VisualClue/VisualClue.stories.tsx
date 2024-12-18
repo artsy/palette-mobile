80a53fc66993eb687ba92b5df65c5a9a415b12a2
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-12-18 12:02:17 -0500

    feat: allow VisualClue color to be customized (#295)

--- src/elements/VisualClue/VisualClue.stories.tsx
+++ src/elements/VisualClue/VisualClue.stories.tsx
@@ -1,11 +1,53 @@
 import { storiesOf } from "@storybook/react-native"
 import { VisualClueDot, VisualClueText } from "./"
 import { List } from "../../storybook/helpers"
+import { Box } from "../Box"
 import { Text } from "../Text"
 
 storiesOf("Theme/Text", module).add("Visual Clue", () => (
   <List>
-    <VisualClueDot />
+    <Box>
+      <Text>Default</Text>
+      <Box position="absolute" top="4px" right="-8px">
+        <VisualClueDot />
+      </Box>
+    </Box>
+
+    <Box>
+      <Text>Diameter: 4</Text>
+      <Box position="absolute" top="4px" right="-4px">
+        <VisualClueDot diameter={4} />
+      </Box>
+    </Box>
+
+    <Box>
+      <Text>Diameter: 8</Text>
+      <Box position="absolute" top="4px" right="-10px">
+        <VisualClueDot diameter={8} />
+      </Box>
+    </Box>
+
+    <Box>
+      <Text>Color: red50</Text>
+      <Box position="absolute" top="4px" right="-8px">
+        <VisualClueDot color="red50" />
+      </Box>
+    </Box>
+
+    <Box>
+      <Text>Color: red100</Text>
+      <Box position="absolute" top="4px" right="-8px">
+        <VisualClueDot color="red100" />
+      </Box>
+    </Box>
+
+    <Box>
+      <Text>Color: black60</Text>
+      <Box position="absolute" top="4px" right="-8px">
+        <VisualClueDot color="black60" />
+      </Box>
+    </Box>
+
     <>
       <Text>A Feature</Text>
       <VisualClueText style={{ top: 14, right: -24 }} />
--- src/elements/VisualClue/VisualClueDot.tsx
+++ src/elements/VisualClue/VisualClueDot.tsx
@@ -1,16 +1,23 @@
 import { StyleProp, ViewStyle } from "react-native"
+import { Color } from "../../types"
 import { useColor } from "../../utils/hooks/useColor"
 import { Flex } from "../Flex"
 
 const DOT_DIAMETER = 6
+const FILL_COLOR: Color = "blue100"
 
 interface VisualClueDotProps {
   diameter?: number
+  color?: Color
   style?: StyleProp<ViewStyle>
 }
 
-export const VisualClueDot: React.FC<VisualClueDotProps> = ({ diameter = DOT_DIAMETER, style }) => {
-  const color = useColor()
+export const VisualClueDot: React.FC<VisualClueDotProps> = ({
+  diameter = DOT_DIAMETER,
+  color = FILL_COLOR,
+  style,
+}) => {
+  const colors = useColor()
 
   return (
     <Flex
@@ -18,7 +25,7 @@ export const VisualClueDot: React.FC<VisualClueDotProps> = ({ diameter = DOT_DIA
         height: diameter,
         minWidth: diameter,
         borderRadius: diameter / 2,
-        backgroundColor: color("blue100"),
+        backgroundColor: colors(color),
         ...(style as object),
       }}
     />
