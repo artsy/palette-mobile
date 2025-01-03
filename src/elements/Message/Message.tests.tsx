5c8acd9599d18a06e1657b692dab0ecdde93f964
Daria Kozlova <Nargle30@gmail.com>
2025-01-03 16:35:40 +0100

    feat(ONYX-1459): add bottom icon position and dark variant to message component  (#297)

--- src/elements/Message/Message.tests.tsx
+++ src/elements/Message/Message.tests.tsx
@@ -32,4 +32,56 @@ describe("Message", () => {
 
     expect(onClose).toHaveBeenCalled()
   })
+
+  it("shows icon component when IconComponent left", () => {
+    renderWithWrappers(
+      <Message
+        variant="default"
+        title="title"
+        text="text"
+        showCloseButton
+        IconComponent={() => <></>}
+      />
+    )
+
+    expect(screen.getByTestId("icon-component-left")).toBeOnTheScreen()
+  })
+
+  it("shows icon component when IconComponent right", () => {
+    renderWithWrappers(
+      <Message
+        iconPosition="right"
+        variant="default"
+        title="title"
+        text="text"
+        showCloseButton
+        IconComponent={() => <></>}
+      />
+    )
+
+    expect(screen.getByTestId("icon-component-right")).toBeOnTheScreen()
+  })
+
+  it("shows icon component when IconComponent bottom", () => {
+    renderWithWrappers(
+      <Message
+        iconPosition="bottom"
+        variant="default"
+        title="title"
+        text="text"
+        showCloseButton
+        IconComponent={() => <></>}
+      />
+    )
+
+    expect(screen.getByTestId("icon-component-bottom")).toBeOnTheScreen()
+  })
+
+  it("does not show icon component when !IconComponent ", () => {
+    renderWithWrappers(<Message variant="default" title="title" text="text" showCloseButton />)
+
+    expect(screen.queryByTestId("icon-component-left")).not.toBeOnTheScreen()
+    expect(screen.queryByTestId("icon-component-right")).not.toBeOnTheScreen()
+    expect(screen.queryByTestId("icon-component-bottom")).not.toBeOnTheScreen()
+  })
 })
