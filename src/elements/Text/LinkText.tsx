209afa8c4f0bb209b0d9d2e2df881c38e81db839
Mounir Dhahri <soussouheros@gmail.com>
2025-02-14 16:54:46 +0100

    chore: more dark mode fixes

--- src/elements/Text/LinkText.tsx
+++ src/elements/Text/LinkText.tsx
@@ -1,5 +1,5 @@
 import { Text, TextProps } from "./Text"
 
 export const LinkText = ({ style, ...props }: TextProps) => (
-  <Text {...props} style={[style, { textDecorationLine: "underline" }]} />
+  <Text color="black100" {...props} style={[style, { textDecorationLine: "underline" }]} />
 )
