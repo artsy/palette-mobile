4d982a6b85ce3b36ec89bc2361ac738f0ee11a74
Ole <ole.richter91@gmail.com>
2025-04-14 11:36:23 +0200

    fix: Increase onBackgroundMedium dark theme color

--- src/tokens.ts
+++ src/tokens.ts
@@ -115,7 +115,7 @@ const v3dark: ThemeWithDarkModeType = {
     background: THEME_DARK.colors.white100,
     onBackground: THEME_DARK.colors.black100,
     onBackgroundHigh: THEME_DARK.colors.black100,
-    onBackgroundMedium: THEME_DARK.colors.black60,
+    onBackgroundMedium: THEME_DARK.colors.black100,
     onBackgroundLow: THEME_DARK.colors.black60,
     surface: "#333",
     onSurface: THEME_DARK.colors.white100,
