8c3d3f10f851c77cfeb063fccb0b9e39ac2454c2
Mounir Dhahri <soussouheros@gmail.com>
2025-04-28 15:49:20 +0200

    feat: support pill tabs

--- src/elements/Tabs/TabsContainer.tsx
+++ src/elements/Tabs/TabsContainer.tsx
@@ -1,3 +1,5 @@
+import { debounce } from "lodash"
+import { useState } from "react"
 import { Platform } from "react-native"
 import {
   Tabs as BaseTabs,
@@ -6,11 +8,13 @@ import {
   MaterialTabItem,
   TabItemProps,
 } from "react-native-collapsible-tab-view"
+import { runOnJS, useAnimatedReaction } from "react-native-reanimated"
 import { DEFAULT_ACTIVE_OPACITY } from "../../constants"
 import { useColor } from "../../utils/hooks/useColor"
 import { useSpace } from "../../utils/hooks/useSpace"
 import { Box } from "../Box"
 import { Flex } from "../Flex"
+import { Pill } from "../Pill"
 
 const TAB_BAR_HEIGHT = 50
 
@@ -19,21 +23,77 @@ interface Indicator {
   Component: React.FC<TabItemProps<string>>
 }
 
+interface PillTabItemProps extends TabItemProps<string> {
+  onTabPress?: (tabName: string) => void
+  focusedTab: { value: string }
+}
+
+const PillTabItem: React.FC<PillTabItemProps> = (props) => {
+  const [focusedTab, setFocusedTab] = useState(props.focusedTab.value)
+
+  // We want to debounce the focusedTab value to avoid showing two pills at once
+  const debouncedFocusedTab = debounce(setFocusedTab, 30)
+
+  useAnimatedReaction(
+    () => props.focusedTab.value,
+    (value) => {
+      runOnJS(debouncedFocusedTab)(value)
+    }
+  )
+
+  return (
+    <Pill
+      selected={props.name === focusedTab}
+      onPress={() => {
+        props.onTabPress?.(props.name)
+      }}
+      key={props.name}
+      variant="link"
+    >
+      {props.name}
+    </Pill>
+  )
+}
+
+interface DefaultTabItemProps extends TabItemProps<string> {
+  indicators: Indicator[]
+  onPress: (name: string) => void
+}
+
+const DefaultTabItem: React.FC<DefaultTabItemProps> = (props) => {
+  const Indicator = props.indicators.find((indicator) => {
+    return indicator.tabName === props.name
+  })
+
+  return (
+    <Box flex={1}>
+      <Flex position="absolute" width="100%">
+        {!!Indicator?.Component && <Indicator.Component {...props} />}
+      </Flex>
+      <MaterialTabItem pressOpacity={DEFAULT_ACTIVE_OPACITY} {...props} onPress={props.onPress} />
+    </Box>
+  )
+}
+
 export interface TabsContainerProps extends CollapsibleProps {
   indicators?: Indicator[]
   // This prop is more immediate than onTabChange, which waits till the
   // transition takes place
   onTabPress?: (tabName: string) => void
+  stickyTabBarComponent?: React.ReactNode
   tabScrollEnabled?: boolean
+  usePills?: boolean
 }
 
 export const TabsContainer: React.FC<TabsContainerProps> = ({
   children,
   indicators = [],
   initialTabName,
-  renderHeader,
-  tabScrollEnabled = false,
   onTabPress,
+  renderHeader,
+  stickyTabBarComponent,
+  tabScrollEnabled = false,
+  usePills = false,
   ...tabContainerProps
 }) => {
   const space = useSpace()
@@ -43,7 +103,6 @@ export const TabsContainer: React.FC<TabsContainerProps> = ({
 
   return (
     <BaseTabs.Container
-      {...tabContainerProps}
       renderHeader={renderHeader}
       headerContainerStyle={{
         shadowOpacity: 0,
@@ -56,6 +115,34 @@ export const TabsContainer: React.FC<TabsContainerProps> = ({
         paddingTop: space(2),
       }}
       renderTabBar={(tabBarProps) => {
+        if (usePills) {
+          return (
+            <Flex flexDirection="row" gap={2} alignItems="center" py={1}>
+              <MaterialTabBar
+                {...tabBarProps}
+                scrollEnabled
+                TabItemComponent={(props) => (
+                  <PillTabItem
+                    {...props}
+                    onTabPress={(tab) => {
+                      onTabPress?.(tab)
+                      tabBarProps.onTabPress(tab)
+                    }}
+                    focusedTab={tabBarProps.focusedTab}
+                  />
+                )}
+                indicatorStyle={{
+                  backgroundColor: "transparent",
+                }}
+                contentContainerStyle={{
+                  gap: space(1),
+                  paddingHorizontal: space(2),
+                }}
+              />
+              {stickyTabBarComponent}
+            </Flex>
+          )
+        }
         return (
           <>
             <MaterialTabBar
@@ -70,24 +157,11 @@ export const TabsContainer: React.FC<TabsContainerProps> = ({
                 borderBottomWidth: 1,
                 borderColor: color("mono30"),
               }}
-              TabItemComponent={(props) => {
-                const Indicator = indicators.find((indicator) => {
-                  return indicator.tabName === props.name
-                })
-
-                return (
-                  <Box flex={1}>
-                    <Flex position="absolute" width="100%">
-                      {!!Indicator?.Component && <Indicator.Component {...props} />}
-                    </Flex>
-                    <MaterialTabItem pressOpacity={DEFAULT_ACTIVE_OPACITY} {...props} />
-                  </Box>
-                )
-              }}
+              TabItemComponent={(props) => <DefaultTabItem {...props} indicators={indicators} />}
               contentContainerStyle={{}}
               activeColor={color("onBackground")}
               inactiveColor={color("onBackgroundMedium")}
-              labelStyle={{ marginHorizontal: 0 }} // removing the horizonal margin from the lib
+              labelStyle={{ marginHorizontal: 0 }} // removing the horizontal margin from the lib
               indicatorStyle={{
                 backgroundColor: color("onBackground"),
                 height: 1,
@@ -103,6 +177,7 @@ export const TabsContainer: React.FC<TabsContainerProps> = ({
           </>
         )
       }}
+      {...tabContainerProps}
     >
       {children}
     </BaseTabs.Container>
