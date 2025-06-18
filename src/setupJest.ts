91a976d99ae0ff95fc1774a0f295b5cb1cbe89f1
George <g.kartalis7@gmail.com>
2025-06-18 18:27:28 +0200

    build(deps): react native testing lib to latest (#355)

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -1,5 +1,3 @@
-import "@testing-library/react-native/extend-expect"
-
 import mockSafeAreaContext from "react-native-safe-area-context/jest/mock"
 jest.mock("react-native-safe-area-context", () => mockSafeAreaContext)
 
