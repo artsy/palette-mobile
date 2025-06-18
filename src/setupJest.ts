9a04321f64d233f239d4a560c3df4db785efce38
Mounir Dhahri <soussouheros@gmail.com>
2025-06-18 18:13:15 +0200

    fix: broken test

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -3,4 +3,11 @@ import "@testing-library/react-native/extend-expect"
 import mockSafeAreaContext from "react-native-safe-area-context/jest/mock"
 jest.mock("react-native-safe-area-context", () => mockSafeAreaContext)
 
+jest.mock("react-native-blurhash", () => {
+  const ReactNative = require("react-native")
+  return {
+    Blurhash: ReactNative.View as any,
+  }
+})
+
 require("react-native-reanimated").setUpTests()
