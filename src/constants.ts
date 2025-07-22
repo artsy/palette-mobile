8b9a6ac653646bebbf291e1a267ac7331c296113
araujobarret <araujobarret@gmail.com>
2025-07-22 11:06:47 +0200

    fix: title overflow

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
+import { PixelRatio } from "react-native"
+
 /**
  * Touch area box for UI elements
  */
 export const DEFAULT_HIT_SLOP = { top: 20, bottom: 20, left: 20, right: 20 }
+
+export const DEFAULT_ICON_SIZE = 24 * PixelRatio.getFontScale()
 
 /**
  * Default active opacity for elements that use Palette's <Touchable />
