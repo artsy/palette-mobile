b218e6df76c02531057b631b4343483dbf2133a1
Mounir Dhahri <soussouheros@gmail.com>
2025-08-08 11:44:23 +0200

    feat: use expo prebuild

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,26 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    resolverMainFields: ["sbmodern", "react-native", "browser", "main"],
-  },
-}
+// const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+//   resolver: {
+//     resolverMainFields: ["sbmodern", "react-native", "browser", "main"],
+//   },
+// }
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+
+const { getDefaultConfig } = require("@expo/metro-config")
+const config = getDefaultConfig(__dirname)
+module.exports = config
