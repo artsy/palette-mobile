b11d774794b9fa094e8909c8f5c83176e35a0e29
George Kartalis <g.kartalis7@gmail.com>
2025-08-08 17:31:30 +0200

    refactor: restructure BodyProps to use discriminated unions for scrollViewProps

--- src/elements/Screen/Body.tsx
+++ src/elements/Screen/Body.tsx
@@ -3,12 +3,25 @@ import { KeyboardAvoidingView, Platform, ScrollView, type ScrollViewProps } from
 import { SCREEN_HORIZONTAL_PADDING } from "./constants"
 import { Flex, FlexProps } from "../Flex"
 
-export interface BodyProps extends FlexProps {
+// Discriminated union: scrollViewProps only permitted when `scroll` is true
+interface BodyBaseProps extends FlexProps {
   fullwidth?: boolean
-  scroll?: boolean
   disableKeyboardAvoidance?: boolean
+  children?: React.ReactNode
+}
+
+interface BodyScrollableProps extends BodyBaseProps {
+  scroll: true
   scrollViewProps?: ScrollViewProps
 }
+
+interface BodyNonScrollableProps extends BodyBaseProps {
+  scroll?: false | undefined
+  // Prevent passing scrollViewProps when not scrolling
+  scrollViewProps?: never
+}
+
+export type BodyProps = BodyScrollableProps | BodyNonScrollableProps
 
 export const Body: React.FC<BodyProps> = ({
   children,
