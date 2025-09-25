d6d126fbb98463a583db8ee4c77150421cbd7b4a
George <g.kartalis7@gmail.com>
2025-09-25 14:50:37 +0200

    build(deps): remove unused dependencies and use expo ts config extension (#386)

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = {
     "@babel/plugin-transform-flow-strip-types",
     ["@babel/plugin-proposal-decorators", { version: "legacy" }],
     ["@babel/plugin-proposal-private-methods", { loose: true }], // needed for latest jest, must come after decorators
-    ["@babel/plugin-proposal-class-properties", { loose: true }], // must come after decorators
     "react-native-reanimated/plugin", // should be LAST
   ],
   presets: [["babel-preset-expo", { jsxRuntime: "automatic" }], "@babel/preset-typescript"],
